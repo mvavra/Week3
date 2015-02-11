7ca95d6a2db2bae8d109f3424814a5aea3611f7d
Michelle Vavra <Michelle@dhcp-164-107-141-44.osuwireless.ohio-state.edu>
2015-02-10 19:01:18 -0500

    Initial Commit

---
+++ Thingy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Thingy
+//
+//  Created by Michelle Vavra on 2/10/15.
+//  Copyright (c) 2015 Michelle Vavra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
